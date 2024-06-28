dc3df8730f716343f78025ce317f1168ba0aee1b
Tudolin <rafael.nadalin@outlook.com>
2024-06-27 21:38:14 -0300

    correção de bugs

--- app.py
+++ app.py
@@ -161,4 +161,4 @@ def relatorios():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, port=8080)
